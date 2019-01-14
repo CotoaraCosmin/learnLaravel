e0026ee916d3b471bff0657608437e9ee18e3b64
cosmin cotoara <cosmin.mail.ro>
2019-01-14 16:53:35 +0200

    all

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,12 +20,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-<<<<<<< HEAD
-Vue.component('appone', require('./components/AppOne.vue').default);
-=======
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('articles', require('./components/Articles.vue').default);
->>>>>>> vueApp-one
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
